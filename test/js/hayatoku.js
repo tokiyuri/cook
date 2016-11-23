da462aeb86150165859c7db1401cdbd0a35f791b
ハタボリアヤナ <a-hatabori@leihauoli.com>
2016-11-24 07:51:15 +0900

    hayatoku.hemlにリネーム hayatoku.css作成 hayatoku.js作成  -スムーススクロール  -一定以上のスクロールで出る

---
+++ test/js/hayatoku.js
@@ -0,0 +1,24 @@
+var $topBtn = $(".goTopBtn");
+
+$(window).load(function(){
+
+	$(function(){
+		$topBtn.hide();
+		$(window).scroll(function(){
+			if($(window).scrollTop() > 0){
+				$topBtn.slideDown(200);
+			}else {
+				$topBtn.slideUp(200);
+			}
+		});
+	});
+	$topBtn.on('click',function(){
+			$('html,body').animate({scrollTop: 0}, 500);
+		return false;
+	});
+});
+
+
+/**
+ * Created by hatabori on 2016/11/21.
+ */
